cc1b4646441d3dc307c3df44b9ca729b68691b09
jzhang <jzhang@xogrp.com>
2018-06-23 18:48:23 +0800

    Finish test

--- lib/bucket.go
+++ lib/bucket.go
@@ -1,30 +1,56 @@
 package lib
 
 import (
-	"bytes"
-	"encoding/binary"
-
-	"github.com/zond/god/murmur"
+	"github.com/spaolacci/murmur3"
 )
 
-// Spilt function
-func Bucket(experimentID string, projectKey string, userID string) int64 {
-	var customerIDBytes bytes.Buffer
-	customerIDBytes.WriteString(experimentID)
-	customerIDBytes.WriteString(projectKey)
-	customerIDBytes.WriteString(userID)
-
-	bucketID := getBucketID(customerIDBytes.String())
-
-	return bucketID
+type Bucket struct {
+	experimentID   string
+	projectKey     string
+	variantVersion string
+	IDRange        map[string]uint64
 }
 
-func getBucketID(customerID string) int64 {
-	var customerIDInt int64
+// Spilt function
+func GetVersion(buckets []Bucket, userID string) string {
+	var variantVersion string
 
-	customerIDHash := murmur.HashString(customerID)
-	binary.Read(bytes.NewReader(customerIDHash), binary.BigEndian, &customerIDInt)
+	bucketID := getBucketID(userID)
 
-	bucketID := customerIDInt & 0xFFFFFFFF % 100
+	for _, bucket := range buckets {
+		if (bucket.IDRange["min"] <= bucketID) && (bucketID <= bucket.IDRange["max"]) {
+			variantVersion = bucket.variantVersion
+		}
+	}
+	return variantVersion
+}
+
+// C
+func CreateBucket(experimentID string, projectKey string, config map[string]uint64) []Bucket {
+	var newBuckets []Bucket
+	startID := uint64(0)
+
+	for key, value := range config {
+		IDRange := make(map[string]uint64)
+		IDRange["min"] = startID
+		IDRange["max"] = startID + value - 1
+		newBuckets = append(newBuckets, Bucket{
+			experimentID,
+			projectKey,
+			key,
+			IDRange,
+		})
+
+		startID = IDRange["max"] + 1
+	}
+
+	return newBuckets
+}
+
+func getBucketID(customerID string) uint64 {
+
+	customerIDint := murmur3.Sum64([]byte(customerID))
+
+	bucketID := customerIDint % 100
 	return bucketID
 }
--- test.go
+++ test.go
@@ -3,10 +3,62 @@ package main
 import (
 	"ab-testing/lib"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	test := lib.Bucket("Test", "test-api-key", "00112233")
+	config := make(map[string]uint64)
+	config["A"] = 60
+	config["B"] = 40
 
-	fmt.Println(test)
+	result := make(map[string]int)
+	result["A"] = 0
+	result["B"] = 0
+	result["C"] = 0
+	result["D"] = 0
+
+	count := 100000000
+
+	fmt.Printf("the percentage of the versions : %v \n", config)
+	fmt.Printf("%v Users are joining this test! \n", count)
+
+	start := time.Now()
+
+	buckets := lib.CreateBucket("Test", "test-api-key", config)
+
+	// var wg sync.WaitGroup
+
+	for i := 0; i < count; i++ {
+		// wg.Add(1)
+		asyncRequestSplit(buckets, result)
+	}
+
+	// wg.Wait()
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Printf("The results: %v \n", result)
+	fmt.Printf("Time spend : %v \n", elapsed)
+}
+
+func asyncRequestSplit(buckets []lib.Bucket, result map[string]int) {
+	ID := RandStringRunes(16)
+	test := lib.GetVersion(buckets, ID)
+	result[test] += 1
+	// wg.Done()
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
